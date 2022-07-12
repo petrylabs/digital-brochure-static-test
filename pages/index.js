cb953b1e262da195bd13eaeca7b053f04ea26dab
Tarakesh Gogi <tgogi@deloitte.ca>
2022-07-12 10:20:16 -0400

    nextjs starter setup

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function IndexPage() {
+  return (
+    <main className="app">
+      <Head>
+        <title>Home page</title>
+      </Head>
+
+      <h1>Hello world</h1>
+    </main>
+  );
+}
