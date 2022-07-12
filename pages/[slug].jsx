4ee4dc696b498dc2a67a7ce057ccf6109c606647
sylvia villegas <svillegas@deloitte.ca>
2022-07-12 15:58:06 -0400

    SKY-186 Create landing page template

---
+++ pages/[slug].jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import Head from "next/head";
+
+export async function getStaticPaths() {
+  // TODO: fetch page slugs from dotCMS
+  const pageSlugs = [
+    "auto-insurance",
+    "condo-insurance",
+    "home-insurance",
+    "tenant-insurance",
+  ];
+
+  const paths = pageSlugs.map((slug) => ({
+    params: {
+      slug,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps() {
+  // TODO: fetch page data from dotCMS & return as props
+
+  return {
+    props: {
+      temporaryPageTitle: "This is a landing page!",
+    },
+  };
+}
+
+/**
+ * Landing page template
+ */
+function LandingPage(props) {
+  const { temporaryPageTitle } = props;
+
+  return (
+    <>
+      {/* CUSTOM PAGE HEAD */}
+
+      <Head>
+        <title>Landing page</title>
+      </Head>
+
+      {/* PAGE TEMPLATE */}
+
+      <h1>{temporaryPageTitle}</h1>
+    </>
+  );
+}
+
+export default LandingPage;
