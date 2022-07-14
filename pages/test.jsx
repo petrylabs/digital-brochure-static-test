56ae56dd4bd86085a42b0e4bafc39f9ec94d4ea9
Tarakesh Gogi <tgogi@deloitte.ca>
2022-07-14 09:41:10 -0400

    added info card component

--- pages/test.jsx
+++ pages/test.jsx
@@ -1,8 +1,11 @@
 import ThreeItemLayout from "../components/ThreeItemLayout";
+import InfoCard from "../components/InfoCard";
 
 function TestComponent() {
   return <div style={{ backgroundColor: `#ccc` }}>This is a component</div>;
 }
+
+const htmlCopy = `<p>Do you lease or finance your vehicle? Having both comprehensive and collision coverage may actually be required by your lender. <snt-link href=\"https://www.sonnet.ca/blog/auto/insurance/lease-finance-insurance\" target=\"_self\"><a href=\"https://www.sonnet.ca/blog/lease-finance-insurance\" title=\"Find out more about insuring a leased or financed car\">Find out more about insuring a leased or financed car</a></snt-link>.</p>`;
 
 /**
  * This is a TEST PAGE for UI components.
@@ -14,9 +17,24 @@ export default function TestPage() {
       <h1>UI TEST PAGE</h1>
 
       <ThreeItemLayout>
-        <TestComponent />
-        <TestComponent />
-        <TestComponent />
+        <InfoCard
+          iconUrl={"https://www.sonnet.ca/dA/6b63420edd/scrollAndFeather.svg"}
+          title={"Hello World"}
+          content={htmlCopy}
+          withBorder={true}
+        ></InfoCard>
+        <InfoCard
+          iconUrl={"https://www.sonnet.ca/dA/6b63420edd/scrollAndFeather.svg"}
+          title={"Hello World"}
+          content={htmlCopy}
+          withBorder={true}
+        ></InfoCard>
+        <InfoCard
+          iconUrl={"https://www.sonnet.ca/dA/6b63420edd/scrollAndFeather.svg"}
+          title={"Hello World"}
+          content={htmlCopy}
+          withBorder={true}
+        ></InfoCard>
         <div>This child will not be displayed!</div>
       </ThreeItemLayout>
     </>
