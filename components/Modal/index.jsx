6360686af0a6416a004c706c1ebf5f75330dd006
sylvia villegas <svillegas@deloitte.ca>
2022-07-14 14:39:49 -0400

    SKY-188 Add close button (non-functional)

--- components/Modal/index.jsx
+++ components/Modal/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Modal as MuiModal } from "@mui/material";
 import PropTypes from "prop-types";
 
+import CloseIcon from "../../icons/CloseIcon";
 import styles from "./Modal.module.scss";
 
 /**
@@ -14,7 +15,14 @@ function Modal(props) {
 
   return (
     <MuiModal open={open} classes={{ root: styles.root }}>
-      <div className={styles.container}>{children}</div>
+      <div className={styles.container}>
+        {/* modal content: */}
+        {children}
+
+        <button type="button" className={styles.closeButton}>
+          <CloseIcon />
+        </button>
+      </div>
     </MuiModal>
   );
 }
