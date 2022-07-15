587c29ea902225b934d259a2cd5e00af4b204e84
nsato <nsato@deloitte.ca>
2022-07-15 15:17:41 -0400

    SKY-195 Fixed conflicts and formatting

--- components/TextSection/index.jsx
+++ components/TextSection/index.jsx
@@ -6,20 +6,19 @@ import styles from "./TextSection.module.scss";
 * TextSection 
 * @docs https://economical.atlassian.net/wiki/spaces/SKT/pages/43179769946/TextSection 
 */ 
-// only one props(json object) comes in 
 
-function TextSection(props) { 
-const hostElement = props.data; 
-const titleString = hostElement[0].headline; 
-const hasCopy = ('copy' in hostElement[0]); 
-const copyString = hasCopy ? hostElement[0].copy: ""; 
+function TextSection(props) {
+  const hostElement = props.data; 
+  const titleString = hostElement[0].headline; 
+  const hasCopy = ('copy' in hostElement[0]); 
+  const copyString = hasCopy ? hostElement[0].copy: ""; 
 
-return (<div className={styles.spacings}> 
-{titleString.length && <h2 className={styles.header}>{titleString}</h2>} 
-{hasCopy && (<div className={`${styles.content}`}>{Parser(copyString)}</div> 
-)} 
-</div> 
-); 
+  return (
+    <div className={styles.spacings}>
+      {titleString.length && <h2 className={styles.header}>{titleString}</h2>} 
+      {hasCopy && (<div className={`${styles.content}`}>{Parser(copyString)}</div>)} 
+    </div> 
+  ); 
 } 
 
 export default TextSection; 
--- pages/test/index.jsx
+++ pages/test/index.jsx
@@ -1,11 +1,11 @@
 import TextSection from "../../components/TextSection/index"; 
 
 const data = [ { 
-"title" : "Auto Insurance / INTRO", 
-"baseType" : "CONTENT", 
-"archived" : false, 
-"copy" : "<p>Getting insured has never been easier with Canada&rsquo;s first online car insurance company. If you live in <a href=\"/auto-insurance/alberta\" target=\"_self\" class=\"\" title=\"Auto Insurance Ontario\">Alberta</a>, <a href=\"/auto-insurance/ontario\" target=\"_self\" class=\"\" hidden-title=\"\" title=\"Auto Insurance Ontario\">Ontario</a>, <a href=\"/auto-insurance/quebec\" target=\"_self\" class=\"\" hidden-title=\"\" title=\"Auto Insurance Qu&eacute;bec\">Québec</a>, New Brunswick, Nova Scotia or P.E.I., you can get a quick quote and buy car insurance online in just a few clicks. Get customized coverage, in language you can understand, at a competitive price &ndash; and do it all online. We&rsquo;ve changed car insurance for the better, so you can worry less and live more.</p>", 
-"headline" : "Get a free car insurance quote and buy online instantly", 
+  "title": "Auto Insurance / INTRO", 
+  "baseType": "CONTENT", 
+  "archived": false, 
+  "copy": "<p>Getting insured has never been easier with Canada&rsquo;s first online car insurance company. If you live in <a href=\"/auto-insurance/alberta\" target=\"_self\" class=\"\" title=\"Auto Insurance Ontario\">Alberta</a>, <a href=\"/auto-insurance/ontario\" target=\"_self\" class=\"\" hidden-title=\"\" title=\"Auto Insurance Ontario\">Ontario</a>, <a href=\"/auto-insurance/quebec\" target=\"_self\" class=\"\" hidden-title=\"\" title=\"Auto Insurance Qu&eacute;bec\">Québec</a>, New Brunswick, Nova Scotia or P.E.I., you can get a quick quote and buy car insurance online in just a few clicks. Get customized coverage, in language you can understand, at a competitive price &ndash; and do it all online. We&rsquo;ve changed car insurance for the better, so you can worry less and live more.</p>", 
+  "headline": "Get a free car insurance quote and buy online instantly", 
 } ]; 
 
 /** 
@@ -14,10 +14,10 @@ const data = [ {
 */ 
 
 export default function TestPage() { 
-return ( 
-<> 
-<TextSection data={data}> 
-</TextSection> 
-</> 
-); 
+  return ( 
+    <> 
+      <TextSection data={data}> 
+      </TextSection> 
+    </> 
+  ); 
 } 
