99526da9ca7293cf929a42751599757ed3ac074e
sylvia villegas <svillegas@deloitte.ca>
2022-07-18 16:12:16 -0400

    Link to sonnet.css fro global styles

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link
+          type="text/css"
+          rel="stylesheet"
+          href="https://www.sonnet.ca/asset/css/sonnet.css"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
