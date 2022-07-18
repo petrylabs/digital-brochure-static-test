77f46435df168772fdabba287b0f7c012baab9eb
sylvia villegas <svillegas@deloitte.ca>
2022-07-18 18:12:08 -0400

    SKY-201 Add some content and styles to QuoteModalContent

--- components/QuoteModalContent/index.jsx
+++ components/QuoteModalContent/index.jsx
@@ -1,11 +1,51 @@
 import React from "react";
 
+import styles from "./QuoteModalContent.module.scss";
+
 /**
  * QuoteModalContent
  * The body of the "Get a quote" modal (displayed inside a `Modal`)
  */
 function QuoteModalContent() {
-  return <div>This is the modal body</div>;
+  return (
+    <>
+      <div className={styles.text}>
+        <h1>Let&apos;s get insured</h1>
+        <p>
+          It only takes 5 minutes. What type of insurance are you looking for?
+        </p>
+      </div>
+
+      <ul className={styles.linksList}>
+        <li>
+          <a href="#" className={styles.link}>
+            {/* link heading here */}
+            {/* icon here */}
+            <span>Home &amp; Auto</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" className={styles.link}>
+            {/* icon here */}
+            <span>Auto</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" className={styles.link}>
+            {/* icon here */}
+            <span>Home</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" className={styles.link}>
+            {/* link heading here */}
+            {/* icon here */}
+            <span>Pet</span>
+          </a>
+        </li>
+      </ul>
+    </>
+  );
 }
 
 export default QuoteModalContent;
