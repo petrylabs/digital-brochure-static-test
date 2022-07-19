4132f6ad546ba62ea4de91961c51660869faa812
Tarakesh Gogi <tgogi@deloitte.ca>
2022-07-19 09:25:03 -0400

    added api methods

--- pages/[slug].jsx
+++ pages/[slug].jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Head from "next/head";
-import PropTypes from "prop-types";
+import PropTypes, { object } from "prop-types";
+import { getPage } from "../utils/api";
 
 export async function getStaticPaths() {
   // TODO: fetch page slugs from dotCMS
@@ -23,12 +24,24 @@ export async function getStaticPaths() {
   };
 }
 
-export async function getStaticProps() {
+export async function getStaticProps({ params }) {
+  const { slug } = params;
   // TODO: fetch page data from dotCMS & return as props
-
+  const { data } = await getPage(slug);
+  if (!data) {
+    console.log("error loop");
+    return {
+      redirect: {
+        destination: "/",
+      },
+    };
+  }
   return {
     props: {
-      temporaryPageTitle: "This is a landing page!",
+      title: data.title,
+      seodescription: data.seodescription,
+      description: data.description,
+      content: data.content,
     },
   };
 }
@@ -37,26 +50,31 @@ export async function getStaticProps() {
  * Landing page template
  */
 function LandingPage(props) {
-  const { temporaryPageTitle } = props;
+  const { title, description, seodescription, content } = props;
 
   return (
     <>
       {/* CUSTOM PAGE HEAD */}
 
       <Head>
-        <title>Landing page</title>
+        <title>{title}</title>
       </Head>
 
       {/* PAGE TEMPLATE */}
 
-      <h1>{temporaryPageTitle}</h1>
+      <h1>{description}</h1>
+
+      <pre>{JSON.stringify(content, null, 2)}</pre>
     </>
   );
 }
 
 LandingPage.propTypes = {
   /** This is just a placeholder prop! To be removed when we have actual props */
-  temporaryPageTitle: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  seodescription: PropTypes.string.isRequired,
+  content: PropTypes.arrayOf(object),
 };
 
 export default LandingPage;
