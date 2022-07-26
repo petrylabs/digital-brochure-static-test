bb2af570a63e399e36b7421c9660940d59c28b99
sylvia villegas <svillegas@deloitte.ca>
2022-07-26 13:45:35 -0400

    SKY-206 Remove SplitLayout form test page

--- pages/test/index.jsx
+++ pages/test/index.jsx
@@ -1,26 +1,13 @@
-import TextSection from "../../components/TextSection/index"; 
+import TextSection from "../../components/TextSection/index";
 import React, { useState } from "react";
 import parse from "html-react-parser";
 import Modal from "../../components/Modal";
 import InfoCard from "../../components/InfoCard";
 import ThreeItemLayout from "../../components/ThreeItemLayout";
 import CTA from "../../components/CTA";
-import SplitLayout from "../../components/SplitLayout";
 import LargeScreenImage from "../../components/LargeScreenImage";
 import placeholderImage from "./placeholder.jpg";
 
-function TestCopyComponent() {
-  return (
-    <>
-      <h2>sunt aut facere repellat provident</h2>
-      <div>
-        quia et suscipit suscipit recusandae consequuntur expedita et cum
-        reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt
-        rem eveniet architecto
-      </div>
-    </>
-  );
-}
 const htmlCopy = `<p>Do you lease or finance your vehicle? Having both comprehensive and collision coverage may actually be required by your lender. <snt-link href=\"https://www.sonnet.ca/blog/auto/insurance/lease-finance-insurance\" target=\"_self\"><a href=\"https://www.sonnet.ca/blog/lease-finance-insurance\" title=\"Find out more about insuring a leased or financed car\">Find out more about insuring a leased or financed car</a></snt-link>.</p>`;
 const parsedCopy = parse(htmlCopy);
 
@@ -66,10 +53,6 @@ export default function TestPage() {
         ></InfoCard>
         <div>This child will not be displayed!</div>
       </ThreeItemLayout>
-      <br />
-      <SplitLayout contentRight={true} imageSRC={placeholderImage}>
-        <TestCopyComponent />
-      </SplitLayout>
 
       <h2>Modal</h2>
       <CTA type="primary" onClick={() => setIsModalOpen(true)}>
@@ -84,8 +67,10 @@ export default function TestPage() {
       <LargeScreenImage src={placeholderImage} width="400" height="300" />
 
       <h2>TextSection</h2>
-      <TextSection titleString={"Get a free car insurance quote and buy online instantly"}
-      copyString={parsedCopyHtml} /> 
+      <TextSection
+        titleString={"Get a free car insurance quote and buy online instantly"}
+        copyString={parsedCopyHtml}
+      />
     </>
   );
 }
