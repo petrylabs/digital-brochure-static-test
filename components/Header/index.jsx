d0ca736fef00a84a67fbbffc714bb870fa867fd6
sylvia villegas <svillegas@deloitte.ca>
2022-07-28 14:55:27 -0400

    SKY-197 Create header expansion panel

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./Header.module.scss";
 
@@ -8,25 +8,47 @@ import styles from "./Header.module.scss";
  * @docs https://economical.atlassian.net/wiki/spaces/SKT/pages/43179900955/Header
  */
 function Header() {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
-    <header className={styles.header}>
+    <header className={styles.header} onMouseLeave={() => setIsExpanded(false)}>
       {/* Skip link */}
-      <a
-        href="#main-content"
-        className={`visually-hidden-focusable ${styles.skipNavLink}`}
-      >
+      <a href="#main-content" className={styles.skipNavLink}>
         {/* TODO: populate from CMS */}
         Skip to main content
       </a>
 
-      {/* Wordmark / Home link */}
-      <a href="https://www.sonnet.ca">Sonnet</a>
+      <div className={`${styles.headerContainer} ${styles.headerBar}`}>
+        {/* Wordmark / Home link */}
+        <a href="https://www.sonnet.ca">Sonnet</a>
 
-      {/* Main Nav */}
-      <div>main nav here</div>
+        {/* Main Nav */}
+        {/* TODO: replace with desktop nav bar */}
+        <div>
+          <button
+            type="button"
+            onMouseEnter={() => setIsExpanded(true)}
+            onClick={() => setIsExpanded(!isExpanded)}
+            aria-controls="expanded-panel"
+            aria-expanded={isExpanded}
+          >
+            nav item
+          </button>
+        </div>
 
-      {/* Secondary Nav */}
-      <div>extra links here</div>
+        {/* Secondary Nav */}
+        <div>extra links here</div>
+      </div>
+
+      {/* Expansion panel */}
+      {isExpanded && (
+        <div
+          id="expanded-panel"
+          className={`${styles.headerContainer} ${styles.headerExpanded}`}
+        >
+          sub-navigation, search input
+        </div>
+      )}
     </header>
   );
 }
