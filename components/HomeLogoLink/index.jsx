fb579dede7f8466d845317eb66e6256e7604b5a3
sylvia villegas <svillegas@deloitte.ca>
2022-08-05 12:43:47 -0400

    SKY-197 Update HomeLogoLink with image

--- components/HomeLogoLink/index.jsx
+++ components/HomeLogoLink/index.jsx
@@ -1,14 +1,29 @@
 import React from "react";
+import Image from "next/image";
+
+import headerData from "../../site-data/header.preval";
+import { baseUrl } from "../../config";
 
 /**
  * HomeLogoLink
  * Displays the Sonnet wordmark as a "home" logo. Used in Desktop & mobile navigation.
  */
 function HomeLogoLink() {
+  const content = headerData.data.headerMenu;
+
   return (
-    <a href="https://www.sonnet.ca">
-      {/* TODO: replace with wordmark logo */}
-      Sonnet
+    <a href={content.logoHref}>
+      <Image
+        // src={`${baseUrl}${content.logoUrl}`}
+        src={`${baseUrl}/dA/b47bb52683/fileAsset/sonnet-wordmark-grey.svg`}
+        layout="fixed"
+        width={100}
+        height={20}
+        alt=""
+      />
+
+      {/* For a11y */}
+      <span className="visually-hidden">Home / Acceuil</span>
     </a>
   );
 }
