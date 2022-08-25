b9051a513303e5b700ecfa1b76df79dba8f6116e
sylvia villegas <svillegas@deloitte.ca>
2022-08-25 10:55:38 -0400

    SKY-199 Update BlogCard to use Blog contentType

--- components/BlogCard/index.jsx
+++ components/BlogCard/index.jsx
@@ -1,58 +1,64 @@
 import React from "react";
-import styles from "./BlogCard.module.scss";
 import Image from "next/image";
 import PropTypes from "prop-types";
 import { customLoader } from "../../utils/images";
 
+import { baseUrl } from "../../config";
+import { imageAlt, imageSrc } from "../../utils/images";
+import styles from "./BlogCard.module.scss";
+
 /**
  * BlogCard
+ * Receives contentType: `Blog` and displays content in a linked card
  * @docs https://economical.atlassian.net/wiki/spaces/SKT/pages/43179311185/BlogCard
  */
 
 function BlogCard(props) {
-  const { imgUrl, imgAltText, title, tag, minRead, link } = props;
+  const { content } = props;
+  const { category, headline, timeToRead, url } = content;
+
+  /* Where does this actually come from? */
+  /* TODO: translate */
+  const tag = Object.keys(category[0]).includes("auto")
+    ? "On the road"
+    : "At home";
+
   return (
-    <a href={link} className={styles.blogLink} ariaLabelledby="blogTitle">
+    <a
+      href={`${baseUrl}${url}`}
+      className={styles.blogLink}
+      aria-labelledby="blogTitle"
+    >
       <div className={styles.blogContainer}>
         <div className={styles.blogImage}>
           <Image
             loader={customLoader}
-            src={imgUrl}
-            alt={imgAltText}
+            src={imageSrc(content, "Blog.blogArticleImage")}
+            alt={imageAlt(content, "Blog.blogArticleImage")}
             layout="fill"
             objectFit="cover"
           />
         </div>
+
         <span className={styles.blogTag}>{tag}</span>
+
         <div className={styles.blogContent}>
           <h3 id="blogTitle" className={styles.blogTitle}>
-            {title}
+            {headline}
           </h3>
         </div>
-        <span className={styles.blogMinRead}>{minRead}</span>
+
+        <span className={styles.blogMinRead}>{timeToRead}</span>
       </div>
     </a>
   );
 }
 
 BlogCard.propTypes = {
-  // image url
-  imgUrl: PropTypes.string.isRequired,
-
-  // image alt text
-  imgAltText: PropTypes.string.isRequired,
-
-  // title for the blog
-  title: PropTypes.string.isRequired,
-
-  // tag for the blog card
-  tag: PropTypes.string.isRequired,
-
-  // min read time
-  minRead: PropTypes.string.isRequired,
-
-  // blog link
-  link: PropTypes.string.isRequired,
+  content: PropTypes.shape({
+    headline: PropTypes.string.isRequired,
+    timeToRead: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default BlogCard;
