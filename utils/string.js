7e26f38b0aa9347e152b463df97697ae7fa22bca
nsato <nsato@deloitte.ca>
2022-09-07 14:29:57 -0400

    SKY-208 Modify replaceToLink and change its name

--- utils/string.js
+++ utils/string.js
@@ -1,3 +1,7 @@
-export const replaceToLink = function (copy) {
+/**
+ * Some cotent(identifier, copy, in introContent in how can I save section) have a link component(snt-link) created by Sonnet team.
+ * The tag needs to be replaced with a tag export function replaceToLink (copy)
+ **/
+export function replaceSntLinkToAtag(copy) {
   return copy.replaceAll("snt-link", "a");
-};
+}
