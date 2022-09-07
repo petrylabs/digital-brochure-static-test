82123c8dae311aadc677ecb7a01542e98b4c1896
nsato <nsato@deloitte.ca>
2022-09-07 17:13:43 -0400

    SKY-208 Modify comment

--- utils/string.js
+++ utils/string.js
@@ -1,6 +1,5 @@
 /**
- * Some cotent(identifier, copy, in introContent in how can I save section) have a link component(snt-link) created by Sonnet team.
- * The tag needs to be replaced with a tag export function replaceToLink (copy)
+ * Replaces <snt-link> in copy(string) with <a> tag
  **/
 export function replaceSntLinkToAtag(copy) {
   return copy.replaceAll("snt-link", "a");
