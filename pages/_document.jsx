199eda263edba7d695f27af8521f1a93e3ffcabe
sylvia villegas <svillegas@deloitte.ca>
2022-09-07 18:41:31 -0400

    SKY-229 Add Script strategy

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -9,6 +9,7 @@ export default function Document() {
         {/* Google Tag Manager -- keep as high as possible in the <head> of the page */}
         <Script
           id="google-tag-manager"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer',${GTMContainerId});`,
           }}
