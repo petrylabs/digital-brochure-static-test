03fc38574904150f35145ba0251a679aeaf7348c
nsato <nsato@deloitte.ca>
2022-09-08 14:58:25 -0400

    SKY-222 Add section in index file and modify api

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,51 @@
 import Head from "next/head";
+import { getPage } from "../utils/api";
+import SplitLayout from "../components/SplitLayout";
 
 /**
  * This is the site homepage.
  */
 
-export default function IndexPage() {
+export async function getStaticProps({ params }) {
+  // const { slug } = params;
+  const { data } = await getPage();
+
+  console.log("data", data);
+
+  // TO DO: create 404 and redirect to it
+  // if (!data) {
+  //   return {
+  //     redirect: {
+  //       destination: "/",
+  //     },
+  //   };
+  // }
+
+  return {
+    props: {
+      title: data.title,
+      seodescription: data.seodescription,
+      description: data.description,
+      content: data.content,
+    },
+  };
+}
+
+export default function IndexPage(props) {
+  const { content } = props;
+  console.log(content);
+
   return (
     <>
       {/* CUSTOM PAGE HEAD */}
-
       <Head>
         <title>Home page</title>
       </Head>
-
-      {/* PAGE TEMPLATE */}
-
       <h1>Hello world</h1>
+      {/* PAGE TEMPLATE */}
+      <section className="bg-white">
+        <SplitLayout content={content[16]} imageRight />
+      </section>
     </>
   );
 }
