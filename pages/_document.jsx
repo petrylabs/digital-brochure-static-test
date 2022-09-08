b082d090f455dcd14e368001207537350088d979
sylvia villegas <svillegas@deloitte.ca>
2022-09-08 13:43:04 -0400

    SKY-229 Remove link for sonnet.css

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -14,12 +14,6 @@ export default function Document() {
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer',${GTMContainerId});`,
           }}
         />
-
-        <link
-          type="text/css"
-          rel="stylesheet"
-          href="https://www.sonnet.ca/asset/css/sonnet.css"
-        />
       </Head>
 
       <body>
