9127dced4a99bf6fb2ee8de989ffda5a757a1907
Harsh Patel <harshptl298@gmail.com>
2022-09-13 15:57:18 -0400

    SKY-219 Added Section-6 with ThreeColumnLayout

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import parse from "html-react-parser";
 
 import { getPage } from "../utils/api";
 import CTAReminderSection from "../components/CTAReminderSection";
@@ -7,6 +8,10 @@ import SplitLayout from "../components/SplitLayout";
 import  ThreeItemLayout from "../components/ThreeItemLayout";
 import BlogCard from "../components/BlogCard";
 import BlogFaqSection from "../components/BlogFaqSection";
+import InfoCard from "../components/InfoCard";
+import LargeScreenImage from "../components/LargeScreenImage";
+import { searchData, getCookie, customLoader } from "../utils";
+
 
 /**
  * This is the site homepage.
@@ -31,6 +36,7 @@ export async function getStaticProps({}) {
 
 export default function IndexPage(props) {
   const { title, content } = props;
+  console.log(content);
   return (
     <>
       {/* CUSTOM PAGE HEAD */}
@@ -43,6 +49,32 @@ export default function IndexPage(props) {
 
       {/* Page hero */}
       <PageHero content={ content[0]} />
+
+      {/* Section 6 */}
+      <section>        
+          <ThreeItemLayout>       
+            <InfoCard 
+              title={content[6].headline} 
+              iconUrl={content[6].fields[0].icon.url} 
+              alt={content[6].fields[0].icon.altText} 
+              content={parse(content[6].copy)}
+              withBorder={true}/>
+              
+            <InfoCard 
+              title={content[7].headline} 
+              iconUrl={content[7].fields[0].icon.url} 
+              alt={content[7].fields[0].icon.altText} 
+              content={parse(content[7].copy)} 
+              withBorder={true}/>
+
+            <LargeScreenImage 
+              src={content[8].fileAsset} 
+              width="400"
+              height="300"
+              loader={customLoader}/>      
+          </ThreeItemLayout>        
+      </section>
+
 
       {/* Blog Section */}
       <BlogFaqSection content={content[9]} blogs={[content[10], content[11], content[12]]}/>
