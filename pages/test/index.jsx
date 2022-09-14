2437aaa74d81be1208493627b5ab4023855a86af
sylvia villegas <svillegas@deloitte.ca>
2022-09-14 16:26:24 -0400

    Remove parse() from test page

--- pages/test/index.jsx
+++ pages/test/index.jsx
@@ -1,6 +1,5 @@
 import TextSection from "../../components/TextSection/index";
 import React, { useState } from "react";
-import parse from "html-react-parser";
 import Modal from "../../components/Modal";
 import InfoCard from "../../components/InfoCard";
 import ThreeItemLayout from "../../components/ThreeItemLayout";
@@ -13,11 +12,7 @@ import Accordion from "../../components/Accordion";
 import AccordionGroup from "../../components/AccordionGroup";
 import { searchData, getCookie, customLoader } from "../../utils";
 
-const htmlCopy = `<p>Do you lease or finance your vehicle? Having both comprehensive and collision coverage may actually be required by your lender. <snt-link href=\"https://www.sonnet.ca/blog/auto/insurance/lease-finance-insurance\" target=\"_self\"><a href=\"https://www.sonnet.ca/blog/lease-finance-insurance\" title=\"Find out more about insuring a leased or financed car\">Find out more about insuring a leased or financed car</a></snt-link>.</p>`;
-const parsedCopy = parse(htmlCopy);
-
-const copyHtml = `<p>Getting insured has never been easier with Canada&rsquo;s first online car insurance company. If you live in <a href="/auto-insurance/alberta" target="_self" class="" title="Auto Insurance Ontario">Alberta</a>, <a href="/auto-insurance/ontario" target="_self" class="" hidden-title="" title="Auto Insurance Ontario">Ontario</a>, <a href="/auto-insurance/quebec" target="_self" class="" hidden-title="" title="Auto Insurance Qu&eacute;bec">Québec</a>, New Brunswick, Nova Scotia or P.E.I., you can get a quick quote and buy car insurance online in just a few clicks. Get customized coverage, in language you can understand, at a competitive price &ndash; and do it all online. We&rsquo;ve changed car insurance for the better, so you can worry less and live more.</p>`;
-const parsedCopyHtml = parse(copyHtml);
+const sampleHtml = `<p>Do you lease or finance your vehicle? Having both comprehensive and collision coverage may actually be required by your lender. <snt-link href=\"https://www.sonnet.ca/blog/auto/insurance/lease-finance-insurance\" target=\"_self\"><a href=\"https://www.sonnet.ca/blog/lease-finance-insurance\" title=\"Find out more about insuring a leased or financed car\">Find out more about insuring a leased or financed car</a></snt-link>.</p>`;
 
 /**
  * This is a TEST PAGE for UI components.
@@ -41,9 +36,9 @@ export default function TestPage() {
 
       <h2>Accordion</h2>
       <AccordionGroup>
-        <Accordion id={"1"} summary={<>Item 1</>} details={parsedCopy} />
-        <Accordion id={"2"} summary={<>Item 2</>} details={parsedCopy} />
-        <Accordion id={"3"} summary={<>Item 3</>} details={parsedCopy} />
+        <Accordion id={"1"} summary={<>Item 1</>} details={sampleHtml} />
+        <Accordion id={"2"} summary={<>Item 2</>} details={sampleHtml} />
+        <Accordion id={"3"} summary={<>Item 3</>} details={sampleHtml} />
       </AccordionGroup>
 
       <h2>ThreeItemLayout &amp; InfoCard:</h2>
@@ -51,18 +46,18 @@ export default function TestPage() {
         <InfoCard
           iconUrl={"https://www.sonnet.ca/dA/6b63420edd/scrollAndFeather.svg"}
           title={"Hello World"}
-          content={parsedCopy}
+          content={sampleHtml}
         ></InfoCard>
         <InfoCard
           iconUrl={"https://www.sonnet.ca/dA/6b63420edd/scrollAndFeather.svg"}
           title={"Hello World"}
-          content={parsedCopy}
+          content={sampleHtml}
           withBorder={false}
         ></InfoCard>
         <InfoCard
           iconUrl={"https://www.sonnet.ca/dA/6b63420edd/scrollAndFeather.svg"}
           title={"Hello World"}
-          content={parsedCopy}
+          content={sampleHtml}
         ></InfoCard>
         <div>This child will not be displayed!</div>
       </ThreeItemLayout>
@@ -87,7 +82,7 @@ export default function TestPage() {
       <h2>TextSection</h2>
       <TextSection
         titleString={"Get a free car insurance quote and buy online instantly"}
-        copyString={parsedCopyHtml}
+        copyString={sampleHtml}
       />
 
       <h2>Shopping cart value</h2>
