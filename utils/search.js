387730d6b417da6f2bd9f8b45841a33f203a6311
sylvia villegas <svillegas@deloitte.ca>
2022-09-15 14:41:49 -0400

    SKY-225 Handle null search term

--- utils/search.js
+++ utils/search.js
@@ -1,6 +1,8 @@
 import searchResults from "../site-data/searchresults.preval";
 
 export const searchData = (searchTerm) => {
+  if (!searchTerm) return null;
+
   const pattern = new RegExp("\\b" + searchTerm, "i");
   return searchResults.data.contentlets.filter((x) => pattern.test(x.title));
 };
