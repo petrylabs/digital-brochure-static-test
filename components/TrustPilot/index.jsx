86cbb6c0513c36de7edd6b7c4485094913885fdf
Nitika <nitika.jindal01@loblaw.ca>
2022-09-20 11:29:46 -0400

    addressed the comment, removed css class and used padding block and inline

--- components/TrustPilot/index.jsx
+++ components/TrustPilot/index.jsx
@@ -6,7 +6,7 @@ import styles from "./TrustPilot.module.scss";
  */
 function TrustPilot() {
     return (
-        <div className={`trustpilot-widget carousel-tp-widget ${styles.container}`} data-locale="en-US" data-template-id="54ad5defc6454f065c28af8b" data-businessunit-id="5e4e0b7438d2d60001f5dc90" data-style-height="240px" data-style-width="100%" data-theme="light" data-tags="CMS-Trust" data-stars="4,5" data-review-languages="en" data-text-color="#323232">
+        <div className={`trustpilot-widget ${styles.container}`} data-locale="en-US" data-template-id="54ad5defc6454f065c28af8b" data-businessunit-id="5e4e0b7438d2d60001f5dc90" data-style-height="240px" data-style-width="100%" data-theme="light" data-tags="CMS-Trust" data-stars="4,5" data-review-languages="en" data-text-color="#323232">
             <a href="https://www.trustpilot.com/review/www.sonnet.ca" target="_blank" rel="noreferrer">Trustpilot</a>
         </div>
     );
