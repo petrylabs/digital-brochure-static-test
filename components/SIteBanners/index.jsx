f5511a061e0e1fd55a7bb8380f33d0c8e6bf0100
sylvia villegas <svillegas@deloitte.ca>
2022-09-28 12:56:02 -0400

    SKY-237 Add SiteBanners structure and style

--- components/SIteBanners/index.jsx
+++ components/SIteBanners/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CloseIcon from "../../icons/CloseIcon";
 import ParsedCopy from "../ParsedCopy";
 import styles from "./SiteBanners.module.scss";
 
@@ -7,18 +8,25 @@ import styles from "./SiteBanners.module.scss";
  * Manages and displays site banners at the top of the page
  */
 function SiteBanners() {
-  const banners = [];
-  const copy = `<p><b>Heads up!</b> Our site is currently unavailable while we make some updates. Sorry for the inconvenience. We will be back up as soon as we can. <a href="#">Learn more</a></p>.`;
+  const copy = `<p><b>Heads up!</b> Our site is currently unavailable while we make some updates. Sorry for the inconvenience. We will be back up as soon as we can. <a href="#">Learn more</a>.</p>`;
+  const banners = [
+    { type: "primary", copy: copy },
+    { type: "secondary", copy: copy },
+    { type: "secondary", copy: copy },
+  ];
 
   return (
-    <section aria-label="notifications">
-      <article className={styles.blue}>
-        <ParsedCopy copy={copy} />
-      </article>
-
-      <article className={styles.grey}>
-        <ParsedCopy copy={copy} />
-      </article>
+    <section aria-label="notifications" className={styles.section}>
+      {banners.map((b, i) => (
+        <article key={i} className={`${styles.banner} ${styles[b.type]}`}>
+          <div>
+            <ParsedCopy copy={b.copy} animatedLinks />
+          </div>
+          <button type="button" className={styles.closeButton}>
+            <CloseIcon />
+          </button>
+        </article>
+      ))}
     </section>
   );
 }
