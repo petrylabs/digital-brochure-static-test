e3bf67f1aa7fe2c3931642dd59ad409184415d14
sylvia villegas <svillegas@deloitte.ca>
2022-10-04 11:54:28 -0400

    SKY-255 Tidy SectionSix component

--- components/SectionSix/index.jsx
+++ components/SectionSix/index.jsx
@@ -7,38 +7,37 @@ import styles from "./SectionSix.module.scss";
 import { baseUrl, breakpoints } from "../../config";
 import { customLoader } from "../../utils";
 
-
 /* Displays Section 6 with one LargeScreenImage on home page */
 
-function SectionSix(props){
-const{content} =props;
-return(
-  <section className={`${styles.threeItemWithLargeScreenImage} ${styles.container}`} style={{ paddingBottom: "13px" }}>
-    <ThreeItemLayout>
+function SectionSix(props) {
+  const { content } = props;
+  return (
+    <section className={styles.section}>
+      <ThreeItemLayout>
         <InfoCard
-            title={content[6].headline}
-            iconUrl={content[6].fields[0].icon.url}
-            alt={content[6].fields[0].icon.altText}
-            content={content[6].copy}
-            withBorder={true}
+          title={content[6].headline}
+          iconUrl={content[6].fields[0].icon.url}
+          alt={content[6].fields[0].icon.altText}
+          content={content[6].copy}
+          withBorder
         />
         <InfoCard
-            title={content[7].headline}
-            iconUrl={content[7].fields[0].icon.url}
-            alt={content[7].fields[0].icon.altText}
-            content={content[7].copy}
-            withBorder={true}
+          title={content[7].headline}
+          iconUrl={content[7].fields[0].icon.url}
+          alt={content[7].fields[0].icon.altText}
+          content={content[7].copy}
+          withBorder
         />
         <LargeScreenImage
-            src={`${baseUrl}${content[8].fileAsset}`}
-            layout="fill"
-            objectFit="cover"
-            loader={customLoader}
-            breakpoint={breakpoints.lg}
+          src={`${baseUrl}${content[8].fileAsset}`}
+          layout="fill"
+          objectFit="cover"
+          loader={customLoader}
+          breakpoint={breakpoints.lg}
         />
-    </ThreeItemLayout>   
-  </section>
-  ) 
+      </ThreeItemLayout>
+    </section>
+  );
 }
 
 export default SectionSix;
