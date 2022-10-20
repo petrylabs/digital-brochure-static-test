d50b1e4cbab714b303c08a3f22b5418c7e2c2408
Tarakesh Gogi <tgogi@deloitte.ca>
2022-10-20 16:48:47 -0400

    fixed nissan missing content

--- pages/[slug].jsx
+++ pages/[slug].jsx
@@ -63,21 +63,22 @@ function LandingPage(props) {
   const { setLanguage } = useContext(LanguageContext);
   const { title, description, seodescription, content, slug } = props;
   const routeInfo = landingPageRoutes?.find((item) => item.path === slug);
-
-  setLanguage(routeInfo.query.locale);
   /* Handle page footer content: */
   const { pageFooterData, setPageFooterData } = useContext(PageFooterContext);
   const legalFooterContent = content.filter(
     (x) => x.contentType === "LegalFooter"
   );
+
   useEffect(() => {
+    setLanguage(routeInfo.query.locale);
     setPageFooterData(legalFooterContent);
   }, []); // empty array, to run once after component mounts
 
   /* Filter out Nissan section for auto page only: */
-  const autoInsurancePage = slug === ("auto-insurance" || "assurance-auto");
-  const nissanSection = content.find((section) =>
-    section?.headline?.includes("Nissan")
+  const autoInsurancePage =
+    slug === "auto-insurance" || slug === "assurance-auto";
+  const nissanSection = content.find(
+    (section) => section?.title === "Nissan Promotion / Auto Landing Page"
   );
   const commonContent = content.filter(
     (section) => !section?.headline?.includes("Nissan")
