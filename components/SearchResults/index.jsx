75b3432878933dab373b3f2ad4412893ceb08d1d
sylvia villegas <svillegas@deloitte.ca>
2022-10-26 12:10:23 -0400

    SKY-263 Strip keyboard behaviour from SearchResults

--- components/SearchResults/index.jsx
+++ components/SearchResults/index.jsx
@@ -7,8 +7,6 @@ import useKeyPress from "../../hooks/useKeyPress";
 import styles from "./SearchResults.module.scss";
 import LanguageContext from "../../context/language";
 
-const initialState = { selectedIndex: 0 };
-
 /**
  * SearchResults
  * Performs a search using provided search term and displays result as a list of links
@@ -18,36 +16,6 @@ function SearchResults(props) {
   const { searchTerm, onResults, onQueryChange } = props;
   const [searchResults, setSearchResults] = useState([]);
 
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const arrowUpPressed = useKeyPress("ArrowUp");
-  const arrowDownPressed = useKeyPress("ArrowDown");
-
-  function reducer(state, action) {
-    // NOTE: Commented code helps replace searchInput when user navigates search
-    // results via arrow keys but needs to be placed elsewhere.
-    // if (searchResults[state.selectedIndex]) {
-    //   onQueryChange(searchResults[state.selectedIndex].title);
-    // }
-    switch (action.type) {
-      case "arrowUp":
-        return {
-          selectedIndex:
-            state.selectedIndex !== 0 ? state.selectedIndex - 1 : 0,
-        };
-      case "arrowDown":
-        return {
-          selectedIndex:
-            state.selectedIndex !== searchResults.length - 1
-              ? state.selectedIndex + 1
-              : 0,
-        };
-      case "select":
-        return { selectedIndex: action.payload };
-      default:
-        throw new Error();
-    }
-  }
-
   /* Perform search as search term changes */
   useEffect(() => {
     const searchResultData = searchData(searchTerm, lang);
@@ -56,20 +24,9 @@ function SearchResults(props) {
     } else {
       setSearchResults([]);
     }
+    console.log(searchResults);
     onResults(searchResultData);
   }, [lang, searchTerm, onResults]);
-
-  useEffect(() => {
-    if (arrowDownPressed) {
-      dispatch({ type: "arrowDown" });
-    }
-  }, [arrowDownPressed, searchResults]);
-
-  useEffect(() => {
-    if (arrowUpPressed) {
-      dispatch({ type: "arrowUp" });
-    }
-  }, [arrowUpPressed, searchResults]);
 
   return searchResults.length > 0 ? (
     <ul
@@ -78,27 +35,8 @@ function SearchResults(props) {
       className={styles.searchResults}
     >
       {searchResults.map((item, i) => (
-        <li
-          role="option"
-          key={i}
-          className={styles.searchResultItem}
-          aria-selected={i == state.selectedIndex}
-          onClick={() => {
-            dispatch({ type: "select", payload: i });
-          }}
-          onKeyPress={reducer}
-          style={{
-            cursor: "pointer",
-          }}
-        >
-          <a
-            href={item.url}
-            className={`${styles.searchResultItemLink} ${
-              i === state.selectedIndex
-                ? styles.searchResultHoveredItem
-                : styles.searchResultItem
-            }`}
-          >
+        <li role="option" key={i} className={styles.searchResultItem}>
+          <a href={item.url} className={styles.searchResultItemLink}>
             {highlight(searchTerm, item.metaTitle || item.title)}
           </a>
         </li>
